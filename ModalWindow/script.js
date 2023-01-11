85168597910f512489b035c88f11b8b6f5f8fb4f
Wayapay <wayapay@Wayapays-MacBook-Pro.local>
2023-01-11 13:51:19 +0300

    add the buttons and js functions

--- ModalWindow/script.js
+++ ModalWindow/script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+for(let i = 0; i < btnOpenModal.length; i++)
+    btnOpenModal(i).addEventListener('click', function(){
+        console.log('Button Clicked');
+    });
+
