ab041875ece9e2b371f902aedec161546e726bf6
Wayapay <wayapay@Wayapays-MacBook-Pro.local>
2023-01-11 14:43:44 +0300

    add the escape button logic

---
+++ ModalWindow/README.md
@@ -0,0 +1,7 @@
+## Modal Window Project
+In this project you will learn the following javascript concepts
+
+1. Query selectors
+2. Event Listener
+3. Keypress event
+4. Check classes in elements
--- ModalWindow/script.js
+++ ModalWindow/script.js
@@ -2,12 +2,39 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btn = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
-for(let i = 0; i < btnOpenModal.length; i++)
-    btnOpenModal(i).addEventListener('click', function(){
-        console.log('Button Clicked');
-    });
+const openModal = function(){
+    console.log("Button Clicked")
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
 
+for(let i = 0; i < btnOpenModal.length; i++)
+    btnOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+
+//Key press event listener
+//keyup - happens when we lift finger off a certain key
+//keypress - fired continuously as we keep our finger on a certain key
+//keydown - Fired as soon as we press down a key
+document.addEventListener('keydown', function(e){
+    console.log("A key was pressed")
+    console.log(typeof(e.key))
+
+    if (e.key == "Escape"){
+        // Check if element contains a class
+        if (!modal.classList.contains('hidden')){
+            closeModal();
+        }
+    }
+})
