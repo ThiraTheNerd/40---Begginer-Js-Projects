0c790e5c0a5740c5872b2d8475106cbfa29ff7ce
Wayapay <wayapay@Wayapays-MacBook-Pro.local>
2023-01-12 22:38:09 +0300

    remove dice after winning

--- PigGame/script.js
+++ PigGame/script.js
@@ -33,49 +33,54 @@ const switchPlayer = function () {
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-    if (playing) {
-        // 1. Generating a random dice roll
-        const dice = Math.trunc(Math.random() * 6) + 1;
-        console.log(dice);
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
-        // 2. Display Dice
-        diceEl.classList.remove('hidden');
-        diceEl.src = `Images/dice-${dice}.png`;
-        //3. Check for rolled 1: if true witch to next player
-        if (dice !== 1) {
-        // Add the dice to the current score
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent =
-            currentScore;
-        } else {
-        // Switch to next player
-        // if active player is 0 then the new active player is 1 else 0
-        switchPlayer();
-        //Add a class that activates player2
-        }
+    // 2. Display Dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `Images/dice-${dice}.png`;
+    //3. Check for rolled 1: if true witch to next player
+    if (dice !== 1) {
+      // Add the dice to the current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      // if active player is 0 then the new active player is 1 else 0
+      switchPlayer();
+      //Add a class that activates player2
     }
+  }
 });
 
 //Holding score functionality
 btnhold.addEventListener('click', function () {
   //1.Add current score to active player
-  scores[activePlayer] += currentScore;
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
 
-  //2. Check if the player score is >= 100
-  if (scores[activePlayer] >= 20) {
-    //Finish game
-    playing = false;
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('player--winner');
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove('player--active');
-  }
-  // Else switch to next player
-  else {
-    switchPlayer();
+    //2. Check if the player score is >= 100
+    if (scores[activePlayer] >= 20) {
+      //Finish game
+      playing = false;
+      diceEl.classList.add('hidden')
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    }
+    // Else switch to next player
+    else {
+      switchPlayer();
+    }
   }
 });
+
+//Restarting the game logic
