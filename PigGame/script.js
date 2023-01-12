5d46e006ce2f536eea12b31255582bad72411601
Wayapay <wayapay@Wayapays-MacBook-Pro.local>
2023-01-12 21:26:38 +0300

    switch active player based on active player

--- PigGame/script.js
+++ PigGame/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Select id elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -32,14 +34,16 @@ btnRoll.addEventListener('click', function(){
         // Add the dice to the current score
         currentScore += dice;
         document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-        current0El.textContent = currentScore;
 
     }else {
         // Switch to next player
         // if active player is 0 then the new active player is 1 else 0
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
         activePlayer = activePlayer === 0 ? 1 :0; 
-
         // Add a class that deactivates player1
+        player0El.classList.toggle('player--active')
+        player1El.classList.toggle('player--active')
         //Add a class that activates player2
     }
 })
